42986b5a66c5e46ae9b13388b7f1bd1844082390
Nekktor <Sok12Nik@yandex.ru>
2026-05-05 20:40:58 +0300

    Копирование Database в папку OnlineServer

---
+++ OnlineServer/Database/api.py
@@ -0,0 +1,2 @@
+from Database.init import Session
+from Database.methods.all_methods import DataBase
---
+++ OnlineServer/Database/methods/init.py
@@ -0,0 +1,5 @@
+from Database.models.users import Users
+from Database.models.chats import Chats
+from Database.models.participants import Participants
+from Database.models.messages import Messages
+from Database.models.friends import Friends
