b05276cfa1390a4d2ba19ef7cb7ec830b23db9f5
Vsevol9d <vsevolod.ignatev11@mail.ru>
2026-05-05 21:04:28 +0300

    Delete Tether-packet directory

--- Tether-packet/Database/api.py
+++
@@ -1,2 +0,0 @@
-from Database.init import Session
-from Database.methods.all_methods import DataBase
--- Tether-packet/Database/methods/init.py
+++
@@ -1,5 +0,0 @@
-from Database.models.users import Users
-from Database.models.chats import Chats
-from Database.models.participants import Participants
-from Database.models.messages import Messages
-from Database.models.friends import Friends
