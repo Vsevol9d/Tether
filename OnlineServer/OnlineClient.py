081db47452f1983fd0b832097ada0306ff946340
Vsevol9d <vsevolod.ignatev11@mail.ru>
2026-05-05 19:05:04 +0300

    Создал папку под онлайн сервер

---
+++ OnlineServer/OnlineClient.py
@@ -0,0 +1,254 @@
+from asyncio import coroutines
+
+import websockets
+import asyncio
+import datetime
+import uuid
+import json
+
+from sqlalchemy.testing import future
+from sqlalchemy.util import await_only
+
+url = "ws://localhost:8765"
+
+
+class TaskManager():
+    def __init__(self):
+        """
+        task_in_progress - задачи в процессе
+        queue_tasks - очередь еще не выполняющихся задач
+        running - запущен ли TaskManager
+        id_response - словарь {id_task : response}
+        """
+
+        self.tasks_in_progress = set()  #
+        self.queue_tasks = asyncio.Queue()
+        self.running = True
+        self.id_response = {}
+
+    async def add_task(self, function, id_task: str, *args, timeout=None) -> asyncio.Future:
+        # print("add_task")
+        """
+        Создание задачи и ожидания ответа на неё
+        :param function: функция, которая делается задачей
+        :param id_task: id задачи
+        :param args: входные данные функции
+        :param timeout: время ожидания в секундах
+        :return: сначало asyncio.Future() - заглушка для реального ответа,
+        когда ответ появился, future становится ответом и возвращается результат в основной код
+        """
+        future = asyncio.Future()
+        self.id_response[id_task] = future
+
+        self.task_info = {"function": function,
+                          "id_task": id_task,
+                          "params": args,
+                          "timeout" : timeout}
+        self.current_task = True
+        await self.queue_tasks.put(self.task_info)
+        return await future
+
+
+    async def start_tasks(self, websocket) -> None:
+        """
+        Запуск TaskManager
+        :param websocket: объект - соединение с пользователем
+        """
+
+        # print("TaskManager запущен")
+        await asyncio.gather(self.process_task(),
+                             self.get_response(websocket))
+
+    async def process_task(self) -> None:
+        """
+        Запускает задачи по мере поступления
+        """
+        while self.running:
+            print(1)
+            # print("1")
+            # print(self.queue_tasks)
+            task_info = await self.queue_tasks.get()
+            # print("2")
+            function = task_info["function"]
+            id_task = task_info["id_task"]
+            params = task_info["params"]
+            timeout = task_info.get("timeout")
+            if timeout:
+                task = asyncio.create_task(
+                    self.run_with_timeout(function(id_task, *params), id_task, timeout)
+                )
+            else:
+                task = asyncio.create_task(function(id_task, *params))
+            # print("3")
+            self.tasks_in_progress.add(task)
+            task.add_done_callback(lambda x: self.remove_task(x))
+            # print("Здача запущена")
+    async def run_with_timeout(self, function, id_task: str, timeout: int):
+        """
+        Ожидает выполнения функции по заданному времени
+
+        :param function: функция
+        :param id_task: id задачи
+        :param timeout: время, после которого задача завершиться
+
+        :return: возвращает awaitable по истечении времени(для запуска внутри другой функции)
+        """
+        try:
+            return await asyncio.wait_for(function, timeout=timeout)
+        except Exception as e:
+            print(f"Задача{id_task} завершилась с ошибкой{e}")
+
+    async def get_response(self, websocket):
+        """
+        Получает все ответы на задачи от сервера, заменяет future на реальное значение ->
+        -> показывает, что ответ на задачу получен
+
+        :param websocket: объект - соединение с пользователем
+        """
+        async for response in websocket:
+            t_response = json.loads(response)
+            print(t_response)
+
+            if t_response["id_task"] in self.id_response:
+                future = self.id_response[t_response["id_task"]]
+                if not future.done():
+                    future.set_result(t_response["response"])
+
+                del self.id_response[t_response["id_task"]]
+
+
+
+            #self.id_response[t_response["id_task"]] = t_response["response"]
+
+    async def stop(self):
+        """
+        Завершает все задачи принудительно
+        """
+        self.running = False
+
+        for task in self.tasks_in_progress:
+            task.cancel()
+
+    def remove_task(self, task):
+        """
+        Callback-функция, удаляет завершившуюся задачу из списка запущенных задач
+
+        :param task: задача
+        Вызывается автоматически при завершении функции
+        """
+        if task in self.tasks_in_progress:
+            self.tasks_in_progress.remove(task)
+
+
+# Создание задачи:
+# await self.task_manager.add_task(self.methodename, str(uuid.uuid4()), websocket, *input_data)
+class Client:
+    def __init__(self):
+        """
+        temp_id - временное id пользователя
+        user_id - постоянное
+        task_manager - TaskManager()
+        """
+        self.temp_id = str(uuid.uuid4())  # временный id
+        self.user_id = ""  # @user_id
+        self.task_manager = TaskManager()
+        self.notifications = set()
+
+    async def connect(self):
+        """
+        Создает соединение с сервером, основное тело клиента
+        """
+        async with websockets.connect(url) as websocket:
+            print("Подключено")
+            try:
+                manager_task = asyncio.create_task(self.task_manager.start_tasks(websocket))
+                await asyncio.sleep(0.1)
+                #
+                #здесь все запуски задач
+                #
+                await  manager_task
+            except Exception as e:
+                print(f"Error: {e}")
+
+    async def get_chats(self, id_task: str, websocket)->None:
+        """
+        Запрос серверу на получение всех чатов
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        """
+        await websocket.send(
+            json.dumps({"action": "get_chats", "id_task": id_task, "params": []}))
+
+
+    async def send_message(self, id_task: str, websocket, message_type: str, text: str, chat_id: str, user_id: str)->None:
+        """
+        Запрос серверу на отправку сообщения
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param message_type: тип сообщения(text, image и другие)
+        :param text: содержимое сообщения(тип - text)
+        :param chat_id: id чата, куда отправляется сообщение
+        :param user_id: id пользователя, который отправил сообщение
+        """
+        await websocket.send(
+            json.dumps({"action": "create_message", "id_task": id_task, "params": [message_type, text, chat_id, user_id]}))
+
+    async def open_chat(self, id_task: str, websocket, user_id: str)->None:
+        """
+        Запрос серверу на получение чата(?)
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param user_id: id пользователя
+        """
+        await websocket.send(
+            json.dumps({"action": "open_chat", "id_task": id_task, "params": [user_id]})
+        )
+
+    async def registration(self, id_task: str, websocket, name: str, username: str, password: str)->None:
+        """
+        Запрос серверу на регистрацию
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param name: имя пользователя
+        :param username: username пользователя(уникальное имя)
+        :param password: пароль
+        """
+        await websocket.send(
+            json.dumps({"action": "registration", "id_task": id_task, "params": [name, username, password]}))
+
+    async def auth(self, id_task: str, websocket, username: str, password: str)->None:
+        """
+        Запрос серверу на авторизацию
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param username: username пользователя(уникальное имя)
+        :param password: пароль
+        """
+        await websocket.send(
+            json.dumps({"action": "auth", "id_task": id_task, "params": [username, password]}))
+
+    async def get_notification(self, id_task: str, websocket, user_id: str)->None:
+        """
+        Запрос на получение уведомлений пользователю
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param user_id: id пользователя
+        """
+        await websocket.send(
+            json.dumps({"action" : "get_notifications", "id_task": id_task, "params": [user_id]}))
+
+    async def clear_note(self)->None:
+        """
+        Отчищает список уведомлений
+        """
+        self.notifications.clear()
+
+if __name__ == "__main__":
+    client = Client()
+    asyncio.run(client.connect())
---
+++ OnlineServer/OnlineSrver.py
@@ -0,0 +1,184 @@
+import json
+
+import websockets
+import asyncio
+from Database.api import Session, DataBase
+import os
+
+class Server():
+    def __init__(self):
+        """
+        connected_clients - словарь {id_user : websocket}
+        action_handlers - типы запросов от клиентов и вызов в зависимости от этого функцию
+        notice - словарь {user_id : список сообщений-уведомлений}
+        """
+
+        self.connected_clients = {}
+
+        #self.clients = set()
+
+        self.action_handlers = {
+            "registration" : self.registration,
+            "auth" : self.auth,
+            "create_message" : self.send_message,
+            "get_chats" : self.get_chats,
+            "open_chat" : self.open_chat,
+            "get_notifications" : self.get_notifications
+        }
+
+        self.notice = {} # user_id : notice[textNotice]
+
+    async def handler(self, websocket)->None:
+        """
+        Функция связи с отдельным пользователем
+        :param websocket: объект - соединение с пользователем(не для заполнения аргумент)
+
+        Экземпляр функции создается для КАЖДОГО отдельного клиента
+        """
+        try:
+            print("Подключено")
+            async for message in websocket:
+
+                t_message = json.loads(message)
+                print(f"1 + {t_message}")
+                await self.action_handlers[t_message["action"]](t_message["id_task"], websocket,  *t_message["params"])
+        finally:
+            for c in self.connected_clients:
+                c.close()
+
+    async def registration(self, id_task: str, websocket, name: str, username: str, password: str)->None:
+        """
+        Функция регистрации пользователя в БД
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param name: имя пользователя
+        :param username: username пользователя(уникальное имя)
+        :param password: пароль
+        """
+        try:
+            out = db.users.exists("username", username)
+            if not out:
+                out = db.users.add(name=name, username=username, password=password)
+                #out = добавление пользователя в БД, получить словарь или ошибку
+            #out = db.users.exists("username", username) # здесь вызвать метода проверки возможности добавления пользователя с пааметрами name, username, lastname (они будут равны = ["Никита2", "Nikitka", "Соколов2"])
+            await websocket.send(json.dumps({"id_task": id_task, "response": out}))
+        except Exception as e:
+            print(f"Error: {e}")
+            await websocket.send(json.dumps({"id_task": id_task, "response": f"Error: {e}"}))
+
+    async def auth(self, id_task: str, websocket, username: str, password: str)->None:
+        """
+        Функция авторизации пользователя
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param username: username пользователя(уникальное имя)
+        :param password: пароль
+        """
+        try:
+            # проверка на свопадения пароля с паролем username out = db.users.exists("username", username)
+
+            out = db.users.exists(username=username, password=password)
+            await websocket.send(json.dumps({"id_task": id_task, "response": out}))
+            self.connected_clients[username] = websocket
+        except Exception as e:
+            print(f"Error: {e}")
+            await websocket.send(json.dumps({"id_task": id_task, "response": f"Fall"}))
+
+    async def send_message(self, id_task: str, websocket, message_type: str, text: str, chat_id: str, user_id: str):
+        """
+        Функция отправки сообщения
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param message_type: тип сообщения(text, image и другие)
+        :param text: содержимое сообщения(тип - text)
+        :param chat_id: id чата, куда отправляется сообщение
+        :param user_id: id пользователя, который отправил сообщение
+        """
+        try:
+            out = db.messages.add(message_type=message_type, text=text, chat_id=chat_id, user_id=user_id)
+
+
+            #добавить уведомление пользователям
+            await websocket.send(json.dumps({"id_task": id_task, "response": out}))
+        except Exception as e:
+            print(f"Error: {e}")
+            await websocket.send(json.dumps({"id_task": id_task, "response": "Fall"}))
+
+    async def get_chats(self, id_task: str, websocket)->None:
+        """
+        Получение всех чатов
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        """
+        try:
+            chats = {}#чаты из БД
+            await websocket.send(json.dumps({"id_task": id_task, "response" : chats}))
+        except Exception as e:
+            print(f"Error: {e}")
+            await websocket.send(json.dumps({"id_task": id_task, "response": "Fall"}))
+
+    async def open_chat(self, id_task: str, websocket, user_id: str)->None:
+        """
+        Получение данных чата(?)
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param user_id: id пользователя
+        """
+        try:
+            out = db.users.exists("")
+            await websocket.send(json.dumps({"id_task": id_task, "response": out}))
+        except Exception as e:
+            print(f"Error: {e}")
+            await websocket.send(json.dumps({"id_task": id_task, "response": "Fall"}))
+
+    def add_notice(self, user_id: str, text: str)->None:
+        """
+        Добавляет уведомление в список уведомлений на отправку
+
+        :param user_id: id пользователя
+        :param text: текст уведомления
+        """
+        if user_id not in self.notice:
+            self.notice[user_id] = []
+
+        self.notice[user_id].append(text)
+
+    async def get_notifications(self, id_task: str, websocket, user_id: str)->None:
+        """
+        Отправляет уведомления пользователю
+
+        :param id_task: id задачи
+        :param websocket: объект - соединение с пользователем
+        :param user_id: id пользователя
+        """
+
+        await websocket.send(
+            json.dumps({"id_task" : id_task, "response" : self.notice[user_id]}))
+
+
+    async def start_server(self)->None:
+        port = int(os.environ.get("PORT", 8765))
+        async with websockets.serve(self.handler, "0.0.0.0", port):
+            print(f"Server started {port}")
+            await asyncio.Future()
+
+
+with Session() as session:
+    db = DataBase(session)
+    try:
+        with session.begin():  # ← одна транзакция на всё
+                # РАБОЧАЯ ЧАСТЬ
+                #db.users.exists("username")
+                if __name__ == "__main__":
+                    server = Server()
+                    asyncio.run(server.start_server())
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+"""if __name__ == "__main__":
+    server = Server()
+    asyncio.run(server.start_server())"""
